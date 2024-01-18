721085d82d704402168a4f05e4122335d80ca8cc
DruDruDru <borisrasgard251@gmail.com>
2024-01-19 00:17:05 +0700

    Передвежение карточек в другие колонки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+let eventBus50 = new Vue()
+let eventBus100 = new Vue()
+
 Vue.component('column', {   
     props: {
         tasks: {
             type: Array,
-            required: false,
+            required: true,
         },
         canAdd: {
             type: Boolean,
@@ -11,7 +14,7 @@ Vue.component('column', {
         addTask: {
             type: Array,
             required: false,
-        }
+        },
     },
     template: `
         <div class="column">
@@ -20,6 +23,7 @@ Vue.component('column', {
                 :key="task.id" 
                 :task="task"
                 @add-point-submitted="addToTask"
+                @on-check="moveToSecond"
             ></card>
             <add-card 
                 v-if="canAdd" 
@@ -61,13 +65,20 @@ Vue.component('column', {
                 }
                 iter++;
             }
+            console.log(this.addTask);
+        },
+        moveToSecond(task) {
+            eventBus50.$emit('on-check', task)
+        },
+        moveToThird(task) {
+            eventBus100.$emit('on-check', task)
         }
     },
     computed: {
         isDisplayForForm() {
             return this.addTask.length < 3
-        }
-    }
+        },
+    },
 })
 
 Vue.component('card', {
@@ -75,7 +86,7 @@ Vue.component('card', {
         task: {
             type: Object,
             required: true
-        }
+        },
     },
     data() {
         return {
@@ -87,12 +98,22 @@ Vue.component('card', {
         <div class="card">
             <div class="titleContainer">
                 <h2>{{ task.title }}</h2>
-                <button type="button" @click="isClick" :disabled="isDisabled" >+</button>
+                <button 
+                    type="button"
+                    @click="isClick" 
+                    :disabled="isDisabled"
+                >+</button>
             </div>
             
             <div v-for="point in task.list">
-                <input type="checkbox" id="..." name="..." value="..."/>
-                <label for="...">{{ point.name }}</label>
+                <input 
+                    type="checkbox" 
+                    :id="point.name" 
+                    :name="point.name" 
+                    class="checkboxInput"
+                    @change="onCheckbox(point)" 
+                />
+                <label :for="point.name">{{ point.name }}</label>
             </div>
 
             <form @submit.prevent="onAddPoint(task.id)" v-show="click" >
@@ -101,11 +122,7 @@ Vue.component('card', {
             </form>
         </div>
     `,
-    computed: {
-        isDisabled() {
-            return this.task.list.length >= 5;
-        }
-    },
+    
     methods: {
         isClick() {
             this.click = true;
@@ -118,7 +135,27 @@ Vue.component('card', {
                 this.click = false
             }
         },
-    }
+        onCheckbox(point) {
+            if (point.checked) point.checked = false; 
+            else point.checked = true;
+
+            checkboxes = document.querySelectorAll('.checkboxInput')
+            console.log(checkboxes);
+
+            count = 0;
+            for (point of this.task.list) {
+                if (point.checked) count++;
+            }
+            let percents = 100 / this.task.list.length * count
+            if (percents >= 50 && percents < 100) eventBus50.$emit('on-check', this.task)
+            if (percents === 100) eventBus100.$emit('on-check', this.task)
+        }
+    },
+    computed: {
+        isDisabled() {
+            return this.task.list.length >= 5;
+        }
+    },
 })
 
 Vue.component('add-card', {
@@ -179,37 +216,84 @@ Vue.component('add-card', {
 let app = new Vue({
     el: '#app',
 
-    data() {
-        return {
-            addInFirstColumn: true,
-            addInSecondColumn: false,
-            addInThirdColumn: false,
-            tasksOfFirst: [
-                {
-                    id: 0,
-                    title: 'Домашка',
-                    list: [
-                        { name: 'Русский язык', checked: false },
-                        { name: 'Алгебра', checked: false },
-                        { name: 'Биология', checked : false }
-                    ],
-                },
-                {
-                    id: 2,
-                    title: 'Домашка',
-                    list: [
-                        { name: 'Русский язык', checked: false },
-                        { name: 'Алгебра', checked: false },
-                        { name: 'Биология', checked : false }
-                    ],
-                }
-            ],
-            tasksOfSecond: [],
-            tasksOfThird: [],
-        }
+    data: {
+        addInFirstColumn: true,
+        addInSecondColumn: false,
+        addInThirdColumn: false,
+        editFirstColumn: true,
+        editSecondColumn: false,
+        editThirdColumn: false,
+        tasksOfFirst: [
+            {
+                id: 0,
+                title: 'Домашка',
+                list: [
+                    { name: 'Русский язык', checked: false },
+                    { name: 'Алгебра', checked: false },
+                    { name: 'Биология', checked : false }
+                ],
+            },
+            {
+                id: 2,
+                title: 'Домашка',
+                list: [
+                    { name: 'Русский язык', checked: false },
+                    { name: 'Алгебра', checked: false },
+                    { name: 'Биология', checked : false }
+                ],
+            }
+        ],
+        tasksOfSecond: [
+            {
+                id: 3,
+                title: 'Домашка',
+                list: [
+                    { name: 'Русский язык', checked: false },
+                    { name: 'Алгебра', checked: false },
+                    { name: 'Биология', checked : false }
+                ],
+            },
+            {
+                id: 4,
+                title: 'Домашка',
+                list: [
+                    { name: 'Русский язык', checked: false },
+                    { name: 'Алгебра', checked: false },
+                    { name: 'Биология', checked : false }
+                ],
+            }
+        ],
+        tasksOfThird: [
+            {
+                id: 5,
+                title: 'Домашка',
+                list: [
+                    { name: 'Русский язык', checked: true },
+                    { name: 'Алгебра', checked: true },
+                    { name: 'Биология', checked : true }
+                ],
+            }
+        ],
     },
     methods: {
-
+        handleOnCheck50(task) {
+            if (this.tasksOfFirst.includes(task) && !(this.tasksOfSecond.includes(task)) && (this.tasksOfSecond.length < 5)) {
+                const idx = this.tasksOfFirst.indexOf(task)
+                this.tasksOfFirst = this.tasksOfFirst.filter((value, index) => idx !== index)
+                this.tasksOfSecond.push(task)
+            }
+        },
+        handleOnCheck100(task) {
+            if (this.tasksOfSecond.includes(task)) {
+                const idx = this.tasksOfSecond.indexOf(task)
+                this.tasksOfSecond = this.tasksOfSecond.filter((value, index) => idx !== index)
+                this.tasksOfThird.push(task)
+            }
+        }
+    },
+    created() {
+        eventBus50.$on('on-check', this.handleOnCheck50),
+        eventBus100.$on('on-check', this.handleOnCheck100)
     }
 });
 
